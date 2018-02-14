5ab7945b903b484358ee89c000467c06672c33d7
SOORAJ <vsooraj@gmail.com>
2018-02-14 07:11:40 +0530

    Login halfway

---
+++ Ivedix.Translator.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ivedix.Translator.Web.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Ivedix.Translator.Web/ViewModels/TranslationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ivedix.Translator.Web.ViewModels
+{
+    public class TranslationViewModel
+    {
+    }
+}
